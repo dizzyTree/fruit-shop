9e1393d81dab19b96799cf3ddd1c1f6e4856a75b
olfili <44113037+olfili@users.noreply.github.com>
2021-04-23 14:53:05 +0200

    hooks demo draft (exkl. onChange and onDestroy)

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../types';
 
@@ -7,14 +7,60 @@ import { Product } from '../types';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+
+export class ProductListComponent implements OnChanges, 
+                                              OnInit, 
+                                              DoCheck, 
+                                              AfterContentInit, 
+                                              AfterContentChecked,
+                                              AfterViewInit,
+                                              AfterViewChecked,
+                                              OnDestroy {
+
   products: Product[];
+  logs: string[];
 
   constructor(private productService: ProductService) {
-    this.products = this.productService.getProducts();
+    this.products = [];
+    this.products = this.products.concat(this.productService.getProducts()[1]);
+    this.logs = [];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.logs.push('OnChanges');
   }
 
   ngOnInit(): void {
+    this.logs.push('OnInit'); 
+  }
+
+  ngDoCheck(): void {
+    this.logs.push('DoCheck'); 
+  }
+
+  ngAfterContentInit(): void {
+    this.logs.push('AfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    this.logs.push('AfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    this.logs.push('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    this.logs.push('AfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    this.logs.push('OnDestroy');
+  }
+
+  buyFruit(element: any) {
+    element.textContent = `Bought!`;
+    element.disabled = true;
   }
 
 }
